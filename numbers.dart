075e9903a46aee5d8bebd05c86407baa76fd31c9
Allen Huber <ash@allenhuber.com>
2024-05-28 14:43:46 -0500

    Add numbers and strings code

---
+++ numbers.dart
@@ -0,0 +1,9 @@
+void main() {
+  int ten = int.parse('10');
+  int hex = 0xABCDEF;
+  var pieLike = double.parse('3.1415');
+  String pieString = pieLike.toString();
+  num num1 = 10;
+  num1 += 0.5;
+  int? badTen = int.tryParse("ten"); // null
+}
---
+++ strings.dart
@@ -0,0 +1,15 @@
+void main() {
+  var str1 = 'I can do single quotes';
+  var str2 = "I can do double quotes too";
+  var str3 = 'I can \'escape\' characters';
+  var str4 = "But this 'escape' is easier";
+
+  var s1 = 'I'
+      'am'
+      'string!';
+  var s2 = 'I' + 'am' + 'string!';
+  var s3 = '''I
+am
+four 
+lines''';
+}
